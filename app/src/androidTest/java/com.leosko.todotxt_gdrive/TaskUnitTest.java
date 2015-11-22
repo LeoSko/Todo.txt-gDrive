7999e9bb7bafb78750abbda4348e7143eca24bee
blacksonik <chegesovdenis@gmail.com>
2015-11-22 16:10:39 +0300

    Added task unit tests

--- app/src/androidTest/java/com.leosko.todotxt_gdrive/TaskUnitTest.java
+++ app/src/androidTest/java/com.leosko.todotxt_gdrive/TaskUnitTest.java
@@ -7,13 +7,54 @@ import junit.framework.TestCase;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import static org.junit.Assert.*;
-
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class TaskUnitTest extends TestCase
 {
+    /*
+    Rule 1: If priority exists, it ALWAYS appears first.
+
+The priority is an uppercase character from A-Z enclosed in parentheses and followed by a space.
+
+For example, this is a task with an A priority:
+
+(A) Call Mom
+
+These tasks have no priority:
+
+    Really gotta call Mom (A) @phone @someday
+    (b) Get back to the boss
+    (B)->Submit TPS report
+
+    Rule 2: A task’s creation date may optionally appear directly after priority and a space.
+
+If there is no priority, the creation date appears first. If the creation date exists, it should be in the format YYYY-MM-DD.
+
+These tasks have creation dates:
+
+    2011-03-02 Document +TodoTxt task format
+    (A) 2011-03-02 Call Mom
+This task doesn’t have a creation date:
+
+(A) Call Mom 2011-03-02
+
+    Rule 3: Contexts and Projects may appear anywhere in the line after priority/prepended date.
+
+A context is preceded by a single space and an @ sign. A project is preceded by a single space and a plus + sign. A project or context contains any non-whitespace character. A task may have zero, one, or more than one projects and contexts included in it.
+
+For example, this task is part of the +Family and +PeaceLoveAndHappiness projects as well as the @iphone and @phone contexts:
+
+(A) Call Mom +Family +PeaceLoveAndHappiness @iphone @phone
+
+This task has no contexts in it:
+
+Email SoAndSo at soandso@example.com
+
+This task has no projects in it:
+
+Learn how to add 2+2
+    */
     @Override
     public void tearDown() throws Exception
     {
@@ -36,15 +77,52 @@ public class TaskUnitTest extends TestCase
         assertEquals(t1.getRawText(), t2.getRawText());
     }
 
-    public void testDate() throws Exception
+    public void testCreatingDateAdding() throws Exception
     {
         Task t1 = new Task("yyyy");
-        assertFalse(t1.hasDate());
-        assertEquals("", t1.getDate());
+        assertFalse(t1.hasCreationDate());
+        assertEquals("", t1.getCreationDate());
         //current date
         String currentDate = new SimpleDateFormat(com.leosko.todotxt_gdrive.TaskEditDialog.DATE_FORMAT).format(new Date());
         t1.setCreationDate(currentDate);
-        assertTrue(t1.hasDate());
-        assertEquals(currentDate, t1.getDate());
+        assertTrue(t1.hasCreationDate());
+        assertEquals(currentDate, t1.getCreationDate());
+    }
+
+    public void testDateRecognition() throws Exception
+    {
+        Task t1 = new Task("2011-03-02 Document +TodoTxt task format");
+        Task t2 = new Task("(A) 2011-03-02 Call Mom");
+        Task t3 = new Task("(A) Call Mom 2011-03-02");
+        assertTrue(t1.hasCreationDate());
+        assertTrue(t2.hasCreationDate());
+        assertFalse(t3.hasCreationDate());
+    }
+
+    public void testCreatingDateChanging() throws Exception
+    {
+        Task t1 = new Task("2015-01-01 selebrate");
+        assertTrue(t1.hasCreationDate());
+        assertEquals("2015-01-01", t1.getCreationDate());
+        t1.setCreationDate("2015-01-02");
+        assertTrue(t1.hasCreationDate());
+        assertEquals("2015-01-02", t1.getCreationDate());
+    }
+
+    public void testPriority() throws Exception
+    {
+        Task t1 = new Task("(A) Do this");
+        Task t2 = new Task("(a) Do that");
+        Task t3 = new Task("do again (b)");
+        assertTrue(t1.hasPriority());
+        assertFalse(t2.hasPriority());
+        assertFalse(t3.hasPriority());
+        // completion shouldn't change the priority
+        t1.changeCompletion();
+        t2.changeCompletion();
+        t3.changeCompletion();
+        assertTrue(t1.hasPriority());
+        assertFalse(t2.hasPriority());
+        assertFalse(t3.hasPriority());
     }
 }
